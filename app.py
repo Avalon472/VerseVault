a6b15de001f763632d858e81deb2cfbd1457f37c
Avalon472 <mjakob478@gmail.com>
2025-10-17 18:20:00 -0500

    Added basic search features and pages

--- app.py
+++ app.py
@@ -1,10 +1,56 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
+from urllib.parse import quote
 
 app = Flask(__name__)
+
+pdb = "https://poetrydb.org"
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/poem")
+def get_poem():
+    title = request.args.get("title")
+
+    r = requests.get(f"{pdb}/title/{title}")
+    if r.status_code != 200:
+        return f"Poem: {title} was not found", 404
+    
+    data = r.json()[0]
+    poem_title = data['title']
+    author = data.get('author')
+    lines = data.get('lines', [])
+
+    return render_template("poem.html", title = poem_title, author = author, lines = lines)
+
+@app.route("/random")
+def get_random():
+
+    r = requests.get(f"{pdb}/random")
+    if r.status_code != 200:
+        return f"Random poem was not found", 404
+    
+    data = r.json()[0]
+    poem_title = data['title']
+    author = data.get('author')
+    lines = data.get('lines', [])
+
+    return render_template("random.html", title = poem_title, author = author, lines = lines)
+
+@app.route("/poet")
+def get_poetWorks():
+    poet = request.args.get("poet")
+
+    r = requests.get(f"{pdb}/author/{poet}/title")
+    if r.status_code != 200:
+        return f"Poet: {poet} could not be found"
+    data = r.json()
+
+    titles = [poem['title'] for poem in data]
+
+    return render_template("poet.html", titles = titles, poet = poet)
 
 if __name__ == "__main__":
     app.run(debug=True)
